d5e69799b8eaa2013d8b2ba4b99f0426aad64d85
ilukianov <lykianovIB@gmail.com>
2016-07-07 20:05:00 +0300

    [DO] CHanged port

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ console.log('isDev = ', isDev);
     module.exports = app;
 
     //  Set the environment variables we need.
-    var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-    var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    var ipaddress = process.env.IP;
+    var port      = process.env.PORT || 8080;
 
     if (typeof ipaddress === "undefined") {
         //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
